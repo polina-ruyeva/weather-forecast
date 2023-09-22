082a97170aebf1305f214dbe7f3321c65532e80a
Polina Ruyeva <60512525+pruyeva@users.noreply.github.com>
2023-09-22 16:41:02 +0200

    add city input (#1)

---
+++ src/lib/components/CityInput.svelte
@@ -0,0 +1,15 @@
+<script>
+    let cityName = '';
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+</script>
+
+<form class="mb-4" on:submit={handleSubmit}>
+    <div class="mb-3">
+        <label for="cityName" class="form-label">Stadtname</label>
+        <input type="text" class="form-control" id="cityName" placeholder="Stadtname eingeben" bind:value={cityName}>
+    </div>
+    <button type="submit" class="btn btn-primary">Wetter abrufen</button>
+</form>
